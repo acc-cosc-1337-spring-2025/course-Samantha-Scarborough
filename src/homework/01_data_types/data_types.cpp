8c94567f3fa1a5f0b9fead9e0e1851c64ac93835
Samantha-Scarborough <samantha.scarborough@g.austincc.edu>
2025-02-15 21:56:24 +0000

    Homework1

--- src/homework/01_data_types/data_types.cpp
+++ src/homework/01_data_types/data_types.cpp
@@ -7,7 +7,11 @@ int add_numbers(int num1, int num2)
 }
 
 //write function code here
-
-
+int multiply_numbers (int num1);
+{
+	int num2 = 5;
+	int product = num1 * num2;
+	return product;
+}
 
 
--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,28 @@
 ﻿//write include statements
-
+#include "data_types.h" //tells c++ where to find multiply_numbers function
+#include <iostream>
 
 //write namespace using statement for cout
-
+using std::cout; using std::cin; 
 
 int main()
 {
+	int num;
+	cout<< "Enter a number: ";
+	cin>>num; 
+
+
+	int result= multiply_numbers(num);
+
+	cout<< "Result: "<<result<<"\n";
+
+	int num1 = 4;
+
+	result = multiply_numbers(num1);
 	
+	cout<< "Result: "<<result<<"\n";
+
+
 
 	return 0;
 }
