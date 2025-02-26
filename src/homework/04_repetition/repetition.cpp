5273a5b9e25349e208d0c25d2abb60553a76d333
Samantha-Scarborough <samantha.scarborough@g.austincc.edu>
2025-02-26 19:24:52 +0000

    Homework4

--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -1,3 +1,39 @@
 //add include statements
+#include <iostream>
+using namespace std;
 
 //add function(s) code here
+// Function to calculate factorial using a loop
+int factorial(int num) {
+    int result = 1;
+    for (int i = 2; i <= num; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+// Function to calculate GCD using a loop (Euclidean algorithm)
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    // Testing the functions
+    int num, a, b;
+
+    cout << "Enter a number to find factorial: ";
+    cin >> num;
+    cout << "Factorial of " << num << " is " << factorial(num) << endl;
+
+    cout << "Enter two numbers to find GCD: ";
+    cin >> a >> b;
+    cout << "GCD of " << a << " and " << b << " is " << gcd(a, b) << endl;
+
+    return 0;
+}
+
