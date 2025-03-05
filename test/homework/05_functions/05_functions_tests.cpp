a116d6f1fc7f097de655ede3e47e912bb5ea5a74
Samantha-Scarborough <samantha.scarborough@g.austincc.edu>
2025-03-05 20:46:46 +0000

    Homework 5

--- test/homework/05_functions/05_functions_tests.cpp
+++ test/homework/05_functions/05_functions_tests.cpp
@@ -5,3 +5,17 @@ TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Verify get_gc_content function"){
+	REQUIRE (content(AGCTATAG)==.375);
+	REQUIRE (content(CGCTATAG)==.50);
+	
+}
+TEST_CASE("Verify get_dna_complement function"){
+	REQUIRE (dna(AGCTATAG)==GATATCGA);
+	REQUIRE (dna(CGCTATAG)==GATATCGC);
+	
+}
+TEST_CASE("Verify reverse_string function"){
+	REQUIRE (reverse(AAAACCCGGT)==ACCGGGTTTT);
+	REQUIRE (reverse(CCCGGAAAAT)==ATTTTCCGGG);
+}
